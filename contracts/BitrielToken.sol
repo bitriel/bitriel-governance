667a20b9a56d78ed9c31628599a0eb654c851e3f
leang27 <52003343+leang27@users.noreply.github.com>
2021-11-16 17:50:05 +0700

    remove interface for BitrielToken

--- contracts/BitrielToken.sol
+++ contracts/BitrielToken.sol
@@ -4,13 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "./interfaces/IBitrielToken.sol";
-
-contract BitrielToken is IBitrielToken, ERC20Votes, Ownable {
+contract BitrielToken is ERC20Votes, Ownable {
     constructor() ERC20("Bitriel", "BTR") ERC20Permit("Bitriel") {}
 
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
-    function mint(address _to, uint256 _amount) public override onlyOwner {
+    function mint(address _to, uint256 _amount) public onlyOwner {
         super._mint(_to, _amount);
     }
 }
--- contracts/interfaces/IBitrielToken.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface IBitrielToken is IERC20 {
-  function mint(address _to, uint256 _amount) external;
-}
